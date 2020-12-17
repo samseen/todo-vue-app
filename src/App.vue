146b6e108b0108e30b2d412d62c0b71b62bfb30d
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-17 19:46:44 +0100

    Hardcoded some todo items.

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,27 @@ export default {
   name: 'App',
   components: {
     
+  },
+  data() {
+    return {
+      todos: [
+        {
+          id: 1,
+          title: "Todo One",
+          completed: false
+        },
+        {
+          id: 2,
+          title: "Todo Two",
+          completed: true
+        },
+        {
+          id: 3,
+          title: "Todo Three",
+          completed: false
+        }
+      ]
+    }
   }
 }
 </script>
