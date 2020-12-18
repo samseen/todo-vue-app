798ec7f6763fd6d7c729a9c6f0d70d6daad8446f
Samson Akanbi <samson.akanbi@outlook.com>
2020-12-18 14:34:49 +0100

    Binded the TodoItem to a class based on the completed property.

--- src/components/TodoItem.vue
+++ src/components/TodoItem.vue
@@ -1,15 +1,29 @@
 <template>
-    <div>
-        <p>Todo Item</p>
+    <div class="todo-item" v-bind:class="{'is-complete':todo.completed}">
+        <p><input type="checkbox" v-on:change="markComplete">
+        {{todo.title}}</p>
     </div>
 </template>
 
 <script>
 export default {
-    name: "TodoItem"
+    name: "TodoItem",
+    props: ["todo"]
 }
 </script>
 
 <style scoped>
-    
+    .todo-item {
+        background: #f4f4f4;
+        padding: 10px;
+        border-bottom: 1px #ccc dotted;
+    }
+
+    .is-complete {
+        text-decoration: line-through;
+    }
+
+    .del {
+        background: #ff0000;
+    }
 </style>
